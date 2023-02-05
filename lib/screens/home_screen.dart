df40ff7d7f9e59840f84561eb17291578fc69e40
amohkaranja <amohdkaranja@gmail.com>
2023-02-05 22:34:25 +0300

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,56 @@
+import 'package:chatapp/widgets/favorite_contacts.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/category_selector.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          )
+        ],
+        elevation: 0.0,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: const Text(
+          'Chats',
+          style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Column(children: <Widget>[
+        const CategorySelector(),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0))),
+            child: Column(
+              children: <Widget>[
+                FavoriteContacts(),
+              ],
+            ),
+          ),
+        )
+      ]),
+    );
+  }
+}
